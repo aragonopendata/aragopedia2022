dfe6d8fbb9d875640f713f228f4af3f89637c597
beni-smartup <bmorales@smartup.es>
2022-11-21 17:22:46 +0100

    Temas definitivos

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -19,6 +19,7 @@ export class ResultsComponent implements OnInit {
   constructor(public temasSvc: TemasService, public timelineSvc: TimeLineSvc) { }
 
 
+  queryUrlResultTemas: string = `https://opendata.aragon.es/sparql?default-graph-uri=&query=select+%3Fs+%3Ftipo+where%0D%0A%7B%0D%0A%7B%0D%0Aselect+distinct+%3Fs+%3Ftipo+%7B%0D%0A%3Fs+%3FpTema+%3Chttp%3A%2F%2Fdatos.gob.es%2Fkos%2Fsector-publico%2Fsector%2Fcultura-ocio%3E.%0D%0A%3Fs+a+%3Ftipo.%0D%0A%3Ftema+skos%3AprefLabel+%3FlabelTema.%0D%0A%0D%0A+VALUES+%3FpTema+%7B+%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Forg%23classification%3E+%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fdcat%23theme%3E+%7D.%0D%0A%0D%0AVALUES+%3Ftipo+%7B+%0D%0A%3Chttp%3A%2F%2Fschema.org%2FCreativeWork%3E%0D%0A%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fdcat%23Dataset%3E%0D%0A%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Forg%23Organization%3E%0D%0A%3Chttp%3A%2F%2Fwww.w3.org%2F2006%2Fvcard%2Fns%23Location%3E%0D%0A%3Chttp%3A%2F%2Fschema.org%2FEvent%3E%0D%0A%3Chttp%3A%2F%2Fpurl.org%2Flinked-data%2Fcube%23dataSet%3E%0D%0A%7D%0D%0A%7D%0D%0A%0D%0A%7D%0D%0Aunion%0D%0A%7B%0D%0Aselect+distinct+%3Fs+%3Ftipo++%7B%0D%0A%0D%0A++%3Chttp%3A%2F%2Fdatos.gob.es%2Fkos%2Fsector-publico%2Fsector%2Fcultura-ocio%3E+a+%3Chttp%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23Concept%3E%3B%0D%0A++++++%3Chttp%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23prefLabel%3E+%3FlabelTema.%0D%0A%0D%0A%0D%0A%3Fs+%3FpTema+%3FlabelTema.%0D%0A%3Fs+a+%3Ftipo.%0D%0A%0D%0A+VALUES+%3FpTema+%7B++%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fdcat%23theme%3E+%7D.%0D%0A%0D%0AVALUES+%3Ftipo+%7B+%0D%0A%3Chttp%3A%2F%2Fwww.w3.org%2Fns%2Fdcat%23Dataset%3E%0D%0A%7D%0D%0A%7D%0D%0A%0D%0A%0D%0A%7D%0D%0A%0D%0A%7D%0D%0A%0D%0Aorder+by+%3Ftipo+%3Fs&format=application%2Fsparql-results%2Bjson&timeout=0&signal_void=on`
 
   active: boolean = false;
   toggleSidebar(): void {
@@ -26,6 +27,7 @@ export class ResultsComponent implements OnInit {
   }
 
   temas: any = [];
+  temasUnicos: any;
 
   filterResult = '';
   results: Result[] = [
@@ -56,16 +58,20 @@ export class ResultsComponent implements OnInit {
 
   ngOnInit(): void {
     this.temasSvc.getTemas().subscribe(data => {
-
       const temasProv = data.results.bindings;
-
       for (let i = 0; i < temasProv.length; i++) {
-        let tema = temasProv[i].tema.value;
-        this.temas.push(tema);
+        let tema = temasProv[i].labelTema.value;
+        this.temas.push(tema)
       }
+      this.temasUnicos = [... new Set(this.temas)];
     });
 
     this.timelineSvc.getCurrentYears();
+
+    this.temasSvc.getResults(this.queryUrlResultTemas).subscribe(data => {
+      console.log(data.results.bindings);
+
+    })
 
   }
 
