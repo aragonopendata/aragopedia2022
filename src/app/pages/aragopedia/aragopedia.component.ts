0ea170a973a705ed14c6a49212e673a23b143445
pmontesinosSmartup <pmontesinos@smartup.es>
2022-11-22 16:35:34 +0100

    inicio aragopedia

---
+++ src/app/pages/aragopedia/aragopedia.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aragopedia',
+  templateUrl: './aragopedia.component.html',
+  styleUrls: ['./aragopedia.component.scss']
+})
+export class AragopediaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
