f07a29ec23c76bb80653a604515e34d22e58120e
beni-smartup <bmorales@smartup.es>
2023-01-19 12:50:46 +0100

    Retoques fichas

--- src/app/pages/aragopedia/aragopedia.component.ts
+++ src/app/pages/aragopedia/aragopedia.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AragopediaComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+
   }
+
 
 }
